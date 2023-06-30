d2fe69109b5118f5ad01c5a52a886aec692dde91
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-06-30 11:20:47 +0300

    Create main camera scene lights

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import * as THREE from 'three';
-import { loadModels, updateAnimation } from './models.js';
+import { loadModels, updateAnimation } from './mesh/models.js';
 
 // Инициализация сцены
 const scene = new THREE.Scene();
 
+scene.fog = new THREE.Fog(0x363d3d, -1, 3000);
+scene.fog.near = 0; // Замените 0 на желаемое значение ближней дальности
+scene.fog.far = 1000; // Замените 1000 на желаемое значение дальней дальности
+
+
+// Initialize the lights.
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const spotLight = new THREE.SpotLight(0xffffff, 0.8);
+spotLight.position.set(200, 200, 0);
+spotLight.castShadow = true;
+scene.add(ambientLight);
+scene.add(spotLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.5); // Попробуйте увеличить интенсивность
+scene.add(hemisphereLight);
+
+
+
 // Инициализация камеры
 const camera = new THREE.PerspectiveCamera(
-  75,
+  80,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  2000
 );
-camera.position.z = 5;
+
+camera.position.set(2, 4, 4.5);
+// camera.position.x = 0; // смещение вправо
+// camera.position.y = 6; // смещение вверх
+// camera.position.z = 8; // смещение назад
 
 // Инициализация рендерера
 const renderer = new THREE.WebGLRenderer();
