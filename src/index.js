ab9e5b569c77fec1fee91ebbac77d37d74b54f6e
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-07-03 20:48:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,22 @@ import { loadModels, updateAnimation } from './mesh/models.js';
 
 // Инициализация сцены
 const scene = new THREE.Scene();
+let stickman; // Переменная stickman
 
 scene.fog = new THREE.Fog(0x363d3d, -1, 3000);
 scene.fog.near = 0; // Замените 0 на желаемое значение ближней дальности
 scene.fog.far = 1000; // Замените 1000 на желаемое значение дальней дальности
 
-
 // Initialize the lights.
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 const spotLight = new THREE.SpotLight(0xffffff, 0.8);
-spotLight.position.set(200, 200, 0);
+spotLight.position.set(-100, 100, 0);
 spotLight.castShadow = true;
 scene.add(ambientLight);
 scene.add(spotLight);
 
 const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.5); // Попробуйте увеличить интенсивность
 scene.add(hemisphereLight);
-
-
 
 // Инициализация камеры
 const camera = new THREE.PerspectiveCamera(
@@ -29,11 +27,6 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   2000
 );
-
-camera.position.set(2, 4, 4.5);
-// camera.position.x = 0; // смещение вправо
-// camera.position.y = 6; // смещение вверх
-// camera.position.z = 8; // смещение назад
 
 // Инициализация рендерера
 const renderer = new THREE.WebGLRenderer();
@@ -49,22 +42,26 @@ window.addEventListener('resize', function () {
   camera.updateProjectionMatrix();
 });
 
-// Загрузка моделей
-loadModels(scene);
+// Загрузка моделей и инициализация игры после загрузки
+loadModels(scene).then(() => {
+  stickman = loadModels.stickman; // Присваивание переменной stickman экспортированного значения из loadModels
+  camera.position.set(1, 4, 5);
 
-// Создание нового экземпляра Clock
-const clock = new THREE.Clock();
+  // Обратить внимание, что позиция камеры устанавливается после загрузки моделей
+  if (stickman) {
+    camera.lookAt(stickman.position);
+  }
 
-// Ваш основной цикл анимации
-function animate() {
+  // Создание нового экземпляра Clock
+  const clock = new THREE.Clock();
+
+  // Ваш основной цикл анимации
+  function animate() {
     requestAnimationFrame(animate);
     const delta = clock.getDelta();
     updateAnimation(delta);
     renderer.render(scene, camera);
-}
+  }
 
-animate();
-
-
-
-
+  animate();
+});
